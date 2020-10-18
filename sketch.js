c1bc461fb54c95b34db9f25bb77c9af92c3ac716
LUSEFER25 <71269868+LUSEFER25@users.noreply.github.com>
2020-10-18 11:23:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	//groundSprite = createSprite(width/2, height-35, width, 10);
+	//groundSprite.shapeColor=color(255,255,0);
+
+	ground = new Ground(width/2, height-35, width, 15);
+	
+	paper = new Paper(100,500,20);
+
+	box_bottom = new Dustbin(600, 650, 200, 20 )
+
+	box_left = new Dustbin(490, 610, 20, 100 )
+
+	box_right = new Dustbin(710, 610, 20, 100 )
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  paper.display();
+  box_bottom.display();
+  box_left.display();
+  box_right.display();
+  ground.display();
+
+
+}
+
+function keyDown()
+{
+	if(keyCode === UP_ARROW)
+	{
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:85, y:-85});
+	}
+}
+
+
+
